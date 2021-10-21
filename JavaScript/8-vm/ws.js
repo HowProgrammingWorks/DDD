d584349883851e375389c59f68452c82c64d51bb
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-10-21 19:02:24 +0300

    Fix `send` calls after moving to ws 8.2.3

--- JavaScript/8-vm/ws.js
+++ JavaScript/8-vm/ws.js
@@ -11,18 +11,18 @@ module.exports = (routing, port) => {
       const obj = JSON.parse(message);
       const { name, method, args = [] } = obj;
       const entity = routing[name];
-      if (!entity) return connection.send('"Not found"');
+      if (!entity) return connection.send('"Not found"', { binary: false });
       const handler = entity[method];
-      if (!handler) return connection.send('"Not found"');
+      if (!handler) return connection.send('"Not found"', { binary: false });
       const json = JSON.stringify(args);
       const parameters = json.substring(1, json.length - 1);
       console.log(`${ip} ${name}.${method}(${parameters})`);
       try {
         const result = await handler(...args);
-        connection.send(JSON.stringify(result.rows));
+        connection.send(JSON.stringify(result.rows), { binary: false });
       } catch (err) {
         console.error(err);
-        connection.send('"Server error"');
+        connection.send('"Server error"', { binary: false });
       }
     });
   });
