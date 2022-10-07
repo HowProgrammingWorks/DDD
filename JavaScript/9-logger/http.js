465ea2edd2fe1f77d4a5a65a886c2cfc878b46a2
Max Levytskyi <immaxlev@gmail.com>
2022-10-07 21:51:48 +0300

    scaffold http & ws switcher on the client side

--- JavaScript/9-logger/http.js
+++ JavaScript/9-logger/http.js
@@ -6,24 +6,20 @@ const receiveArgs = async (req) => {
   const buffers = [];
   for await (const chunk of req) buffers.push(chunk);
   const data = Buffer.concat(buffers).toString();
-  return JSON.parse(data);
+  return JSON.parse(data).data;
 };
 
 module.exports = (routing, port) => {
   http.createServer(async (req, res) => {
     const { url, socket } = req;
-    const [name, method, id] = url.substring(1).split('/');
+    const [name, method] = url.substring(1).split('/');
     const entity = routing[name];
     if (!entity) return res.end('Not found');
     const handler = entity[method];
     if (!handler) return res.end('Not found');
-    const src = handler.toString();
-    const signature = src.substring(0, src.indexOf(')'));
-    const args = [];
-    if (signature.includes('(id')) args.push(id);
-    if (signature.includes('{')) args.push(await receiveArgs(req));
+    const args = await receiveArgs(req);
     console.log(`${socket.remoteAddress} ${method} ${url}`);
-    const result = await handler(...args);
+    const result = await handler(...Array.isArray(args) ? args : [args]);
     res.end(JSON.stringify(result.rows));
   }).listen(port);
 
--- JavaScript/9-logger/static/client.js
+++ JavaScript/9-logger/static/client.js
@@ -1,8 +1,6 @@
 'use strict';
 
-const socket = new WebSocket('ws://127.0.0.1:8001/');
-
-const scaffold = (structure) => {
+function scaffoldIterator(structure, handler) {
   const api = {};
   const services = Object.keys(structure);
   for (const serviceName of services) {
@@ -10,20 +8,55 @@ const scaffold = (structure) => {
     const service = structure[serviceName];
     const methods = Object.keys(service);
     for (const methodName of methods) {
-      api[serviceName][methodName] = (...args) => new Promise((resolve) => {
-        const packet = { name: serviceName, method: methodName, args };
-        socket.send(JSON.stringify(packet));
-        socket.onmessage = (event) => {
-          const data = JSON.parse(event.data);
-          resolve(data);
-        };
-      });
+      api[serviceName][methodName] = handler(serviceName, methodName, structure[serviceName][methodName]);
     }
   }
-  return api;
+}
+
+const scaffoldHttp = (url, structure) => {
+  const api = {};
+  scaffoldIterator(structure, (serviceName, methodName) => {
+    if (!api[serviceName]) {
+      api[serviceName] = {}
+    }
+    api[serviceName][methodName] = (...args) => fetch(`${url}/${serviceName}/${methodName}`,{
+      method: 'POST',
+      body: JSON.stringify({ data: args })
+    })
+  })
+  return Promise.resolve(api);
+}
+
+const scaffoldWs = (url, structure) => {
+  const socket = new WebSocket(url);
+  const api = {};
+  scaffoldIterator(structure, (serviceName, methodName) => {
+    if (!api[serviceName]) {
+      api[serviceName] = {}
+    }
+    api[serviceName][methodName] = (...args) => new Promise((resolve) => {
+      const packet = { name: serviceName, method: methodName, args };
+      socket.send(JSON.stringify(packet));
+      socket.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        resolve(data);
+      };
+    });
+  })
+  return new Promise(resolve => {
+    socket.addEventListener('open', async () => {
+      resolve(api)
+    });
+  });
 };
 
-const api = scaffold({
+function scaffold(url, structure) {
+  return url.startsWith('ws') ? scaffoldWs(url, structure) : scaffoldHttp(url, structure)
+}
+
+let api;
+
+scaffold('http://127.0.0.1:8001',{
   user: {
     create: ['record'],
     read: ['id'],
@@ -36,9 +69,4 @@ const api = scaffold({
     delete: ['id'],
     find: ['mask'],
   },
-});
-
-socket.addEventListener('open', async () => {
-  const data = await api.user.read(3);
-  console.dir({ data });
-});
+}).then(result => api = result)
