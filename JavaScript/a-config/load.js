cdb7f92e5c098a1d16f338909ed2cf1a3f782c87
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-10-07 22:21:09 +0300

    Remove debug output

--- JavaScript/a-config/load.js
+++ JavaScript/a-config/load.js
@@ -7,9 +7,7 @@ module.exports = (options) => async (filePath, sandbox) => {
   const src = await fs.readFile(filePath, 'utf8');
   const code = `'use strict';\n${src}`;
   const script = new vm.Script(code);
-  console.log({ script });
   const context = vm.createContext(Object.freeze({ ...sandbox }));
   const exported = script.runInContext(context, options);
-  console.log({ exported });
   return exported;
 };
