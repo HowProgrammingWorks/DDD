be75a2c3e2ca93316a0d1c42b0bb0f2a1ecb29a3
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-10-07 18:06:23 +0300

    Server-side transport strategy

---
+++ JavaScript/b-transport/transport/ws.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const { Server } = require('ws');
+
+module.exports = (routing, port, console) => {
+  const ws = new Server({ port });
+
+  ws.on('connection', (connection, req) => {
+    const ip = req.socket.remoteAddress;
+    connection.on('message', async (message) => {
+      const obj = JSON.parse(message);
+      const { name, method, args = [] } = obj;
+      const entity = routing[name];
+      if (!entity) return connection.send('"Not found"', { binary: false });
+      const handler = entity[method];
+      if (!handler) return connection.send('"Not found"', { binary: false });
+      const json = JSON.stringify(args);
+      const parameters = json.substring(1, json.length - 1);
+      console.log(`${ip} ${name}.${method}(${parameters})`);
+      try {
+        const result = await handler(...args);
+        connection.send(JSON.stringify(result.rows), { binary: false });
+      } catch (err) {
+        console.error(err);
+        connection.send('"Server error"', { binary: false });
+      }
+    });
+  });
+
+  console.log(`API on port ${port}`);
+};
