921d326d265f27d8b9ac91da9dc1eed0fcd9bf90
Max Levytskyi <immaxlev@gmail.com>
2022-10-08 21:05:43 +0300

    add http headers

--- JavaScript/9-logger/transports/http.js
+++ JavaScript/9-logger/transports/http.js
@@ -2,6 +2,16 @@
 
 const http = require('node:http');
 
+const HEADERS = {
+  'X-XSS-Protection': '1; mode=block',
+  'X-Content-Type-Options': 'nosniff',
+  'Strict-Transport-Security': 'max-age=31536000; includeSubdomains; preload',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Content-Type': 'application/json; charset=UTF-8',
+};
+
 const receiveArgs = async (req) => {
   const buffers = [];
   for await (const chunk of req) buffers.push(chunk);
@@ -11,6 +21,7 @@ const receiveArgs = async (req) => {
 
 module.exports = (routing, port) => {
   http.createServer(async (req, res) => {
+    res.writeHead(200, HEADERS);
     const { url, socket } = req;
     const [name, method] = url.substring(1).split('/');
     const entity = routing[name];
