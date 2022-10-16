219a71a6d9e1f92898bab3f1a79c02f975d10ad7
DarkInsider <jeka19982405@gmail.com>
2022-10-16 11:44:28 +0300

    chore(src): init new app

---
+++ JavaScript/b-pluggable-framework/db.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const pg = require('pg');
+
+const crud = (pool) => (table) => ({
+  async query(sql, args) {
+    const result = await pool.query(sql, args);
+    return result.rows;
+  },
+
+  async read(id, fields = ['*']) {
+    const names = fields.join(', ');
+    const sql = `SELECT ${names} FROM ${table}`;
+    if (!id) return pool.query(sql);
+    return pool.query(`${sql} WHERE id = $1`, [id]);
+  },
+
+  async create({ ...record }) {
+    const keys = Object.keys(record);
+    const nums = new Array(keys.length);
+    const data = new Array(keys.length);
+    let i = 0;
+    for (const key of keys) {
+      data[i] = record[key];
+      nums[i] = `$${++i}`;
+    }
+    const fields = '"' + keys.join('", "') + '"';
+    const params = nums.join(', ');
+    const sql = `INSERT INTO "${table}" (${fields}) VALUES (${params})`;
+    return pool.query(sql, data);
+  },
+
+  async update(id, { ...record }) {
+    const keys = Object.keys(record);
+    const updates = new Array(keys.length);
+    const data = new Array(keys.length);
+    let i = 0;
+    for (const key of keys) {
+      data[i] = record[key];
+      updates[i] = `${key} = $${++i}`;
+    }
+    const delta = updates.join(', ');
+    const sql = `UPDATE ${table} SET ${delta} WHERE id = $${++i}`;
+    data.push(id);
+    return pool.query(sql, data);
+  },
+
+  async delete(id) {
+    const sql = 'DELETE FROM ${table} WHERE id = $1';
+    return pool.query(sql, [id]);
+  },
+});
+
+module.exports = (options) => crud(new pg.Pool(options));
