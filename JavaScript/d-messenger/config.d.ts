1b0deffa202d9abf78d7b0fa547b024976586795
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-12-09 09:38:15 +0200

    Remove config and console from global namespace

--- JavaScript/d-messenger/config.d.ts
+++ JavaScript/d-messenger/config.d.ts
@@ -1,4 +1,4 @@
-interface Config {
+export interface Config {
   static: {
     port: number;
   };
--- JavaScript/d-messenger/global.d.ts
+++ JavaScript/d-messenger/global.d.ts
@@ -1,10 +1,7 @@
 import * as Common from './lib/common';
-import * as Config from './config';
 
 declare global {
   namespace api {}
-  const console: Console;
-  const common: typeof Common;
-  const config: typeof Config;
   namespace db {}
+  const common: typeof Common;
 }
