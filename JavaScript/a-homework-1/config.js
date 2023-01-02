d71950ae8be38c66b1a795a41326813f6eab43fc
eugenemtn <e.mitnik@gmail.com>
2023-01-02 14:58:31 +0100

    otask 1: create config file

---
+++ JavaScript/a-homework-1/config.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const config = {
+  ports: {
+    api: 8001,
+    static: 8000,
+  },
+  transport: "ws",
+  db: {
+    host: "127.0.0.1",
+    port: 5432,
+    database: "example",
+    user: "ubuntu",
+    password: "ubuntu",
+  },
+};
+
+module.exports = config;
