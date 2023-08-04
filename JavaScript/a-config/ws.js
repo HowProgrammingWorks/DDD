eb568901e46c3600829a77abdf6d27e74d85f5c3
RohovAlex <47148269+RohovAlex@users.noreply.github.com>
2023-08-04 16:03:13 +0300

    changes in a-config example

--- JavaScript/a-config/ws.js
+++ JavaScript/a-config/ws.js
@@ -26,7 +26,7 @@ module.exports = (routing, port) => {
       console.log(`${ip} ${name}.${method}(${parameters})`);
       try {
         const result = await handler(...args);
-        connection.send(JSON.stringify(result.rows), { binary: false });
+        connection.send(JSON.stringify(result), { binary: false });
       } catch (err) {
         console.error(err);
         connection.send('"Server error"', { binary: false });
