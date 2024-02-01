3a02b6b06f1e9545ab744a39a3d818630bbeada7
Roman B <kotfly@gmail.com>
2024-02-01 23:45:25 +0400

    Implements client-side http transport
    * Adds support of node 20 to package.json

--- JavaScript/9-logger/static/client.js
+++ JavaScript/9-logger/static/client.js
@@ -1,8 +1,44 @@
 'use strict';
 
-const socket = new WebSocket('ws://127.0.0.1:8001/');
+const API_URL = 'ws://localhost:8001';
 
-const scaffold = (structure) => {
+const getTransport = (url) => {
+  let socket;
+  const protocol = url.split(':')[0];
+
+  const transports = {
+    ws: (name, method) => {
+      if (!socket) socket = new WebSocket(url);
+      return (...args) => {
+        return new Promise((resolve) => {
+          const packet = { name, method, args };
+          socket.send(JSON.stringify(packet));
+          socket.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            resolve(data);
+          };
+        });
+      };
+    },
+
+    http:
+      (name, method, methodArgs) =>
+      (...args) => {
+        const urlParts = [url, name, method];
+        if (methodArgs[0] === 'id') urlParts.push(args.shift());
+
+        return fetch(urlParts.join('/'), {
+          method: 'POST',
+          body: JSON.stringify(args),
+        }).then((response) => response.json());
+      },
+  };
+
+  return transports[protocol];
+};
+
+const scaffold = (url, structure) => {
+  const transport = getTransport(url);
   const api = {};
   const services = Object.keys(structure);
   for (const serviceName of services) {
@@ -10,20 +46,17 @@ const scaffold = (structure) => {
     const service = structure[serviceName];
     const methods = Object.keys(service);
     for (const methodName of methods) {
-      api[serviceName][methodName] = (...args) => new Promise((resolve) => {
-        const packet = { name: serviceName, method: methodName, args };
-        socket.send(JSON.stringify(packet));
-        socket.onmessage = (event) => {
-          const data = JSON.parse(event.data);
-          resolve(data);
-        };
-      });
+      api[serviceName][methodName] = transport(
+        serviceName,
+        methodName,
+        service[methodName],
+      );
     }
   }
   return api;
 };
 
-const api = scaffold({
+const api = scaffold(API_URL, {
   user: {
     create: ['record'],
     read: ['id'],
@@ -31,14 +64,23 @@ const api = scaffold({
     delete: ['id'],
     find: ['mask'],
   },
+  city: {
+    read: ['id'],
+    create: ['record'],
+    update: ['id', 'record'],
+    delete: ['id'],
+  },
   country: {
     read: ['id'],
+    create: ['record'],
+    update: ['id', 'record'],
     delete: ['id'],
-    find: ['mask'],
   },
 });
 
-socket.addEventListener('open', async () => {
-  const data = await api.user.read(3);
-  console.dir({ data });
-});
+// socket.addEventListener('open', async () => {
+//   const data = await api.user.read(3);
+//   console.dir({ data });
+// });
+
+(async () => console.log(await api.user.read(3)))();
